ac446ebd26dc41a3aa436fcd41a516999e85a7ec
OrangeeZ <ultramegacrasher@gmail.com>
2015-12-14 01:53:49 +0300

    + Random NPC spawner & NPC list + StatusModifier loader

--- Assets/Scripts/Spawner/NPCSpawner.cs
+++ Assets/Scripts/Spawner/NPCSpawner.cs
@@ -1,19 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NPCSpawner : AObject
-{
-	public NPCInfo info;
+public class NPCSpawner : AObject {
 
-	private void Start()
-	{
-		var view = Instantiate(info.groundView, position: this.position);
-		view.npc = info;
+	public NPCListInfo NpcListInfo;
+
+	private NPCInfo _currentInfo;
+
+	private void Start() {
+
+		_currentInfo = NpcListInfo.Infos.RandomElement();
+
+		var view = Instantiate( _currentInfo.groundView, position: this.position );
+		view.npc = _currentInfo;
 	}
 
 	private void OnValidate() {
-		name = string.Format( "NPC Spawner [{0}]", info == null ? "null" : info.name );
+
+		name = string.Format( "NPC Spawner [{0}]", _currentInfo == null ? "null" : _currentInfo.name );
 	}
 
 }
-
