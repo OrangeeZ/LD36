04923b52b8ca63014e554708c7079dc581351026
Fedor Eftimitsa <all.games.regs@gmail.com>
2015-12-13 12:33:21 +0300

    button for load data

---
+++ Assets/Scripts/Editor/RemoteDalaLoaderEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(RemoteDataLoader))]
+public class RemoteDalaLoaderEditor : Editor
+{
+	public override void OnInspectorGUI ()
+	{
+		base.OnInspectorGUI ();
+
+		if (GUILayout.Button("Load")) {
+			((RemoteDataLoader)target).LoadRemoteData();
+		}
+	}
+}
+
