fa19408114e8f7b9f0297c8c8e2bbbb3c4a722aa
andrey.zaycev <dondrakone89@mail.ru>
2015-12-14 19:10:59 +0300

    Minor changes

--- Assets/Scripts/EnviromentRandomizer.cs
+++ Assets/Scripts/EnviromentRandomizer.cs
@@ -15,7 +15,7 @@ public class EnviromentRandomizer : MonoBehaviour
         {
             
             float randRotate = Random.Range(0f, 360f);
-            float randScale = Random.Range(-0.1f, 0.1f);
+            float randScale = Random.Range(-0.3f, 0.3f);
             trees[i].transform.Rotate(0f, randRotate, 0f);
             trees[i].transform.localScale += new Vector3(randScale, randScale, randScale);
 
