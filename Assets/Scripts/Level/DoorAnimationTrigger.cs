a63ed993ed499d33a7e85c624758335088ebf370
Mefodei Link <mefodei.link@gmail.com>
2016-04-17 19:33:49 +0300

    test tile room

---
+++ Assets/Scripts/Level/DoorAnimationTrigger.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Level
+{
+    public class DoorAnimationTrigger : MonoBehaviour
+    {
+        private List<Animator> _animators = new List<Animator>();
+
+        public bool IsOpen { get; protected set; }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            SetDoorState(collision.collider, true);
+        }
+
+        private void OnCollisionStay(Collision collisionInfo)
+        {
+            SetDoorState(collisionInfo.collider, true);
+        }
+
+        private void OnCollisionExit(Collision collision)
+        {
+            SetDoorState(collision.collider, false);
+        }
+
+        //private methods
+
+        private void SetDoorState(Collider collider, bool state)
+        {
+            if (IsOpen == state || collider == null) return;
+            var component = collider.GetComponent<CharacterPawn>();
+            if (component == null) return;
+            IsOpen = true;
+            _animators.ForEach(x => x.SetBool("Open", IsOpen));
+        }
+
+        private void Awake()
+        {
+            gameObject.GetComponentsInChildren<Animator>(true, _animators);
+        }
+
+    }
+}
---
+++ Assets/Scripts/TileEditor/TileMapController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TileMapController : MonoBehaviour
+{
+    public Text _saveFileName;
+    public Text _loadFileName;
+    public IsometricMapGenerator IsometricMapGenerator;
+
+    public void Save()
+    {
+        IsometricMapGenerator.SaveMap(_saveFileName.text);
+    }
+
+    public void Load()
+    {
+        IsometricMapGenerator.LoadMap(_loadFileName.text);
+    }
+}
