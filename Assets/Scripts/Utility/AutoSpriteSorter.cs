6bff6b3888e198857b6e57f78dc0fa41c64a542e
OrangeeZ <ultramegacrasher@gmail.com>
2016-04-18 01:51:29 +0300

    + AutoSpriteSorter ~ Updated sprite sorting layers

---
+++ Assets/Scripts/Utility/AutoSpriteSorter.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoSpriteSorter : MonoBehaviour {
+
+	[ContextMenu( "Sort" )]
+	private void Sort() {
+#if UNITY_EDITOR
+		var sprites = GetComponentsInChildren<SpriteRenderer>();
+		foreach ( var each in sprites ) {
+
+			each.sortingOrder = Mathf.RoundToInt( -each.transform.position.z * 10f );
+
+			UnityEditor.EditorUtility.SetDirty( each );
+		}
+#endif
+	}
+
+}
