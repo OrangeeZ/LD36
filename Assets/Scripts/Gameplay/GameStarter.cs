85c1cabff18fe9d75f7ea4190521dff323f05ab6
OrangeeZ <ultramegacrasher@gmail.com>
2016-04-18 12:16:56 +0300

    + Level loader

---
+++ Assets/Scripts/Gameplay/GameStarter.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameStarter : MonoBehaviour {
+
+	[SerializeField]
+	private Object[] _scenesToLoad;
+
+	// Use this for initialization
+	void Start () {
+
+		foreach ( var each in _scenesToLoad ) {
+			
+			SceneManager.LoadScene( each.name, LoadSceneMode.Additive );
+		}
+
+		SceneManager.UnloadScene( "Loader" );
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
