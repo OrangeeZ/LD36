6ea7139c7e409dcd6acd78e527954684fd6a9c77
OrangeeZ <ultramegacrasher@gmail.com>
2016-04-18 23:21:56 +0300

    + Shock gun effect

--- Assets/Scripts/Effects/EffectSpawner.cs
+++ Assets/Scripts/Effects/EffectSpawner.cs
@@ -8,10 +8,17 @@ using UnityStandardAssets.Effects;
 public class EffectSpawner : AObject {
 
 	public GameObject CharacterDeathEffect;
+	public GameObject ShockGunEffect;
 
 	private void Start() {
 
 		EventSystem.Events.SubscribeOfType<Character.Died>( OnCharacterDie );
+		EventSystem.Events.SubscribeOfType<RangedWeaponInfo.RangedWeapon.Fire>( OnWeaponFire );
+	}
+
+	private void OnWeaponFire( RangedWeaponInfo.RangedWeapon.Fire eventObject ) {
+
+		Instantiate( ShockGunEffect, eventObject.Character.Pawn.position, Quaternion.FromToRotation( Vector3.right, eventObject.Weapon.AttackDirection ) );
 	}
 
 	private void OnCharacterDie( Character.Died diedEvent ) {
